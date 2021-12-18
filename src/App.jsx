ad6d403ddb3ad9ca3e63b17c7c6359d9d5e6747a
amansharma8194 <amankumarsharma8194031598@gmail.com>
2021-12-18 15:03:56 +0530

    setup ready

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => (
+  <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>it's working today i'm going to complete unit 2.</p>
+    <p>hello my name is aman</p>
+  </>
+);
